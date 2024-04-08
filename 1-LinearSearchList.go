441d7352ee39af03172a32b625cb313e5a016b17
KurobaneShin <darkflamemaster663@gmail.com>
2024-04-08 20:42:22 -0300

    make type more legible

--- 1-LinearSearchList.go
+++ 1-LinearSearchList.go
@@ -1,7 +1,6 @@
 package main
 
-func LinearSearchList[S ~[]E, E comparable](list S, item E) bool {
-
+func LinearSearchList[List ~[]Item, Item comparable](list List, item Item) bool {
 	for _, val := range list {
 		if val == item {
 			return true
